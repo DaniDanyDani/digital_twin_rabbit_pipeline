8ce3e79d183756bae9a67b578656dfe2b751b601
Daniel Leme <daniel.moreira.leme@gmail.com>
2026-04-17 02:27:40 -0300

    Add: Leitura de ecg raw e salvar o ecg e o tanque

--- read_and_save_RawEcg.py
+++ read_and_save_RawEcg.py
@@ -4,8 +4,8 @@ from app.io_functions import load_ecg_from_mat, save_ecg_to_csv, export_mesh_to_
 meu_ecg = load_ecg_from_mat('data/input/raw/data-sinus.mat')
 
 # 2. Lógica: Processa em memória RAM
-meu_ecg.preprocess(fs_target=4000)
+meu_ecg.preprocess(fs_target=1000, time_window=[40, 120])
 
 # 3. I/O: Salva no disco
-save_ecg_to_csv(meu_ecg, output_dir='data/output/pre_process_raw', base_name='coelho_4000hz')
+save_ecg_to_csv(meu_ecg, output_dir='data/output/pre_process_raw', base_name='coelho_1000hz_40-120ms')
 export_mesh_to_vtk(meu_ecg, output_dir='data/output/mesh')
